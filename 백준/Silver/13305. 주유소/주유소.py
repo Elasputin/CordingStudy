4e87804e8f35668fd35a12b245a94504f434ad34
truffleToast <135800603+truffleToast@users.noreply.github.com>
2023-09-16 23:03:55 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 46584 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,14 +1,12 @@
 n = int(input())
-distances = list(map(int, input().split()))
-prices = list(map(int, input().split()))
-
-min_price = prices[0]  # 첫 도시의 가격을 최소 가격으로 초기화
-total_cost = 0
-
-for i in range(n - 1):
-    if prices[i] < min_price:  # 현재 도시의 가격이 최소 가격보다 작다면 업데이트
-        min_price = prices[i]
-    total_cost += min_price * distances[i]
-
-print(total_cost)
-
+doro = list(map(int, input().split()))
+price = list(map(int, input().split()))
+min_price = price[0] #첫번째 값을 설정
+cost = 0
+for i in range(n-1):
+    if price[i] < min_price: #price[i] 가 min_price보다 작다면
+        min_price = price[i]
+    cost += doro[i] * min_price
+print(cost)
+        
+        
